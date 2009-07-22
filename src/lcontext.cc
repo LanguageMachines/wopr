83b468f62255e26ac80826104e2c2d65cc27b399
(no author) <(no author)@12f355fe-0486-481a-ad91-c297ab22b4e3>
2009-07-22 08:46:28 +0000

    added lcontext files.
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/wopr@2756 12f355fe-0486-481a-ad91-c297ab22b4e3

---
+++ src/lcontext.cc
@@ -0,0 +1,47 @@
+
+// ---------------------------------------------------------------------------
+// $Id: lcontext.cc 2426 2009-01-07 12:24:00Z pberck $
+// ---------------------------------------------------------------------------
+
+/*****************************************************************************
+ * Copyright 2007 - 2009 Peter Berck                                         *
+ *                                                                           *
+ * This file is part of wopr.                                                *
+ *                                                                           *
+ * wopr is free software; you can redistribute it and/or modify it           *
+ * under the terms of the GNU General Public License as published by the     *
+ * Free Software Foundation; either version 2 of the License, or (at your    *
+ * option) any later version.                                                *
+ *                                                                           *
+ * wopr is distributed in the hope that it will be useful, but WITHOUT       *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or     *
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License      *
+ * for more details.                                                         *
+ *                                                                           *
+ * You should have received a copy of the GNU General Public License         *
+ * along with wpred; if not, write to the Free Software Foundation,          *
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA               *
+ *****************************************************************************/
+
+// ---------------------------------------------------------------------------
+//  Includes.
+// ---------------------------------------------------------------------------
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <string>
+#include <iostream>
+
+#include "qlog.h"
+#include "Config.h"
+#include "util.h"
+#include "lcontext.h"
+
+// ---------------------------------------------------------------------------
+//  Code.
+// ---------------------------------------------------------------------------
+
+
+// ---------------------------------------------------------------------------
