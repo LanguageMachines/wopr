cc8e9d1a72487feba9d80ba3af3dec8200a0c0e8
pberck <pberck@12f355fe-0486-481a-ad91-c297ab22b4e3>
2009-12-22 09:25:22 +0000

    Updated md(2) script.
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/wopr@3401 12f355fe-0486-481a-ad91-c297ab22b4e3

--- etc/examine_md.pl
+++ etc/examine_md.pl
@@ -18,6 +18,19 @@ use Getopt::Std;
 #  00 0 break
 #  00 0 to
 #  00 0 humiliate
+#  ...
+#  00: 20/0         both wrong/combined_correct
+#  01: 1/0          second right/combined_correct
+#  10: 4/3          first right ...
+#  11: 1/1          both right ...
+#
+# Output for md2 file:
+#  00 Cady's        (0=wrong, 1=right) for each classiier,
+#  00 passion       followed by target
+#  10 for
+#  ...
+#  0: 7/1           classifier 0: 1 right out of 7
+#  1: 7/0           classifier 1: 0 right out of 7
 #
 #------------------------------------------------------------------------------
 
@@ -28,6 +41,11 @@ getopts('f:');
 my $file = $opt_f || 0;
 
 #------------------------------------------------------------------------------
+
+# All classifiers is binary from 0..n
+
+my %scores_count;
+my %scores_right;
 
 open(FH, $file) || die "Can't open file.";
 
@@ -45,6 +63,7 @@ while ( my $line = <FH> ) {
   my $target;
   my $best;
   my $best_token;
+  my $binary = "";
   my (@i_parts, @c_parts, @b_parts);
 
   if ( $line =~ /(.*) \[ (.*) \] \{ (.*) \}/ ) {
@@ -58,8 +77,10 @@ while ( my $line = <FH> ) {
     for ( my $i = 0; $i <= $#c_parts;  $i += 2 ) {
       if ( $c_parts[$i] eq $target ) {
 	print "1";
+	$binary .= "1";
       } else {
 	print "0";
+	$binary .= "0";
       }
     }
 
@@ -67,8 +88,10 @@ while ( my $line = <FH> ) {
     @b_parts  = split (/ /, $best);
     $best_token = $b_parts[0];
     
+    ++$scores_count{$binary};
     if ( $best_token eq $target ) {
       print " 1";
+      ++$scores_right{$binary};
     } else {
       print " 0";
     }
@@ -81,19 +104,32 @@ while ( my $line = <FH> ) {
     $classifications = $2;
     @c_parts  = split (/ /, $classifications);
     for ( my $i = 0; $i <= $#c_parts;  $i += 2 ) {
+      ++$scores_count{$i/2};
       if ( $c_parts[$i] eq $target ) {
 	print "1";
+	++$scores_right{$i/2};
       } else {
 	print "0";
+	$binary .= "0";
       }
     }
   }
   print " $target\n";
 }
 
-#printf( "correct guess: %8i (%5.2f)\n", $cg, $cg/$total*100 );
-#printf( "correct dist.: %8i (%5.2f)\n", $cd, $cd/$total*100 );
-#printf( "correct total: %8i (%5.2f)\n", ($cd+$cg), ($cd+$cg)/$total*100 );
-#printf( "incorrect    : %8i (%5.2f)\n", $ic, $ic/$total*100 );
-#printf( "total guesses: %8i\n", $total );
+foreach my $key (sort (keys(%scores_count))) {
+  print "$key: $scores_count{$key}/";
+  if ( defined $scores_right{$key} ) {
+    print $scores_right{$key};
+  } else {
+    print "0";
+  }
+  print "\n";
+}
+
+
+#11: 950/950
+#01: 613/51
+#00: 16490/69
+#10: 4776/4415
 
