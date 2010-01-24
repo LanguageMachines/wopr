8fccbaa96a56c8c0c7f3077899df327ac98197cd
pberck <pberck@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-01-24 10:59:39 +0000

    Yet another script added.
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/wopr@3561 12f355fe-0486-481a-ad91-c297ab22b4e3

--- etc/compare_ngp_srilm.pl
+++ etc/compare_ngp_srilm.pl
@@ -6,36 +6,22 @@ use Getopt::Std;
 
 #------------------------------------------------------------------------------
 #
-# Shows the pplx, pplx(nOOV), ppl, ppl1 from wopr and SRILM
-#
-# perl compare_pxs_srilm.pl -f nyt.tail1000.l2r0_20100121a_2e7.pxs 
-#                  -s nyt.tail1000.ngramlm_yt.2e7.srilm.dbg2.out -l 2 
-#  84.99   84.99   61.35   61.35
-#  85.77   55.10  178.77  178.77
-# 189.28  189.28  111.53  111.53
-#
-# compare_pxs_srilm.pl ... > foo
-# gnuplot
-# plot [0:99][0:2000] "foo" using 2 w li, "foo" using 3 with lines
-#
 #------------------------------------------------------------------------------
 
-use vars qw/ $opt_f $opt_s $opt_v $opt_l $opt_r /;
+use vars qw/ $opt_f $opt_s $opt_v /;
 
-getopts('f:s:vl:r:');
+getopts('f:s:v');
 
 my $wopr_file  = $opt_f || 0;
 my $srilm_file = $opt_s || 0;
 my $verbose    = $opt_v || 0;
-my $lc         = $opt_l || 0;
-my $rc         = $opt_r || 0;
 
 #------------------------------------------------------------------------------
 
 my %summary;
-my $log2prob_pos = $lc + $rc + 2;
-my $target_pos   = $lc + $rc + 1;
-my $unknown_pos  = $lc + $rc + 5;
+
+my $log2prob_pos  = 0;
+my $wopr_pplx_pos = 1;
 
 open(FHW, $wopr_file)  || die "Can't open file.";
 open(FHS, $srilm_file) || die "Can't open file.";
@@ -59,20 +45,13 @@ while ( my $line = <FHS> ) {
 	my $wline = get_next_wopr();
 	my @parts  = split (/ /, $wline);
 
-	my $sum_l2p        += $parts[2];
-	my $text_wordcount += $parts[1];
-	my $sum_noov_l2p   += $parts[6];
-	my $text_noov_wordcount += $parts[5];
+	my $wopr_ppl  = $parts[$wopr_pplx_pos]; #ngp is like SRILM, no OOV
 
-	#printf( "%4d ", $idx++ );
-	my $wopr_ppl  = 2**(-$parts[2]/$parts[1]);
-	my $wopr_ppl1 = 2**(-$parts[6]/$parts[5]); #comparable to SRILM
-
-	printf( "%7.2f %7.2f ", $wopr_ppl, $wopr_ppl1 );
+	printf( "%7.2f ", $wopr_ppl );
 	printf( "%7.2f %7.2f\n", $srilm_ppl, $srilm_ppl1 );
 	
 	my $indicator = 0;
-	my $ratio = $wopr_ppl1 / $srilm_ppl;
+	my $ratio = $wopr_ppl / $srilm_ppl;
 	if ( $ratio >= 2 ) {
 	    $indicator += 1;
 	} elsif ( $ratio < 0.5 ) {
@@ -131,8 +110,7 @@ The Carlyle Group figures prominently in Unger's book .
 1 sentences, 9 words, 0 OOVs
 0 zeroprobs, logprob= -24.6601 ppl= 292.419 ppl1= 549.549
 
-pxs output:
-# nr. #words sum(log2prob) avg.pplx avg.wordlp #nOOV sum(nOOVl2p) std.dev(wordlp) [wordlp(each word)]
-0 16 -156.428 877.178 317962 14 -114.199 807377 [ 10.1829 2.26994e+06 7642.88 162138 14 2.26994e+06 42829 2.94444 8918 49.5457 324276 7 1560.09 60 3 3.8 ]
-1 15 -139.682 635.677 591539 14 -118.568 629021 [ 7.29817 712.417 771.038 48.1919 2.26994e+06 18.2207 11.7504 378322 6 21.5395 567484 567484 18.2207 814.007 51 ]
-2 7 -73.9057 1507.52 1.60474e+06 7 -73.9057 857812 [ 2.26994e+06 18916.1 60 70935.5 127.267 143.839 5.28926 ]
+ngp3 output:
+-114.882 295.259 16 2 The Plastics seem fascinated with Cady's passion for school and ignorance of American popular culture .
+-114.913 295.717 15 1 `` You're like a Martian , '' Regina , the Alpha Plastic , says .
+-68.2872 864.258 7 0 Cady becomes an experiment for them .
