a1b0e38c4e998043deb7918af83706909d4dd201
pberck <pberck@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-05-21 08:53:12 +0000

    Wopr script added.
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/wopr@4692 12f355fe-0486-481a-ad91-c297ab22b4e3

--- etc/compare_mg_px.pl
+++ etc/compare_mg_px.pl
@@ -70,3 +70,7 @@ px output:
 _ _ The `` -3.34807 8.24174 10.1829 cd 1 0 9103 [ `` 14787 The 10598 But 4149 In 2877 He 2869 ]
 _ The Plastics Derby -21.1142 10.5035 2.26994e+06 icu 2 1 3236 [ Derby 152 New 111 company 96 first 81 two 74 ]
 
+mg output:
+
+eat nothing more [ dflt 0 to 0.0531758 D cd 1 0 ] 155 2031 { to 306 }
+nothing more Mrs [ dflt 0 to 0 D ic 2 1 ] 15 96 { to 36 }
