fb1fdd8a821460069bec1fb4e848ccd0a8f98e82
pberck <pberck@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-06-09 14:21:17 +0000

    New script, and fixes.
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/wopr@4994 12f355fe-0486-481a-ad91-c297ab22b4e3

---
+++ etc/do_all_mg.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+TRAINFILE="rmt.1e5"
+TESTFILE="rmt.test.1e3"
+DEVFILE="rmt.dev.1e3"
+FOCUS="rmt.1e5.lex"
+#
+WOPR="/exp/pberck/wopr/wopr"
+CYCLE=0
+#
+for FFC in 1000 10000 50000
+do
+  for K in {1..2}
+  do
+    SCRIPT="do_compare_mg_px.script"
+    if test $K -eq 2
+    then
+      SCRIPT="do_compare_mg_px.script_k2"
+    fi
+    CYCLE=$(( $CYCLE + 1 ))
+    ID=RUN${CYCLE}.k${K}.ffc${FFC}
+    echo "${WOPR} -l -s ${SCRIPT} -p filename:${TRAINFILE},testfile:${TESTFILE},devfile:${DEVFILE},focus:${FOCUS},ffc:${FFC},id:${ID}"
+    RES=`${WOPR} -l -s ${SCRIPT} -p filename:${TRAINFILE},testfile:${TESTFILE},devfile:${DEVFILE},focus:${FOCUS},ffc:${FFC},id:${ID} > output.${ID}`
+  done
+done
