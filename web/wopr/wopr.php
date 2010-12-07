4854e5c930984d4fb783238be270a067a8b55405
pberck <pberck@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-12-07 10:26:41 +0000

    JS de/encode quotes fixed.
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/wopr@7167 12f355fe-0486-481a-ad91-c297ab22b4e3

--- web/wopr/wopr.php
+++ web/wopr/wopr.php
@@ -15,15 +15,24 @@ function get_get_value($name) {
   return "";
 }
 
+// 0 is off, otherwise on.
+//
+$mq = get_magic_quotes_gpc();
+
 $host = get_post_value('host');
 $port = get_post_value('port');
+$txt  = get_post_value('txt');
 
-$txt = get_post_value('txt');
+if ( $mq !== 0 ) {
+  $txt = stripslashes($txt);
+}
 
 // Get rid of the linefeeds. Should I replace them with a token,
-// and have Wopr divide into seperate sentences?
+// and have Wopr divide into seperate sentences? Quotes...?
 //
 $txt = trim( preg_replace( '/\s+/', ' ', $txt ) );
+//$txt = preg_replace( '/\\\"/',   '__DBLQ__', $txt );
+//$txt = preg_replace( '/\\\\\'/', '__QUOT__', $txt ); //urgh
 
 $cmd = get_post_value('cmd');
 
@@ -40,9 +49,12 @@ if ( $socket ) {
   stream_set_timeout($socket, 15);
 
   if ( ! feof($socket) ) { // if was while.
-    echo fgets( $socket );
+    //echo fgets( $socket );
+    $str = fgets( $socket );
+    echo $str;
   }
-  fwrite( $socket, "ok\n" );
+  fclose($h);
+
   $info = stream_get_meta_data($socket);
   if ($info['timed_out']) {
     echo "<error><msg>Timeout</msg></error>";
