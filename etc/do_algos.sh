506bb7d093e6d5c0fdd67d77441bfa5bb304726d
pberck <pberck@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-12-08 12:32:26 +0000

    Regexps added to script.
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/wopr@7212 12f355fe-0486-481a-ad91-c297ab22b4e3

--- etc/do_algos.sh
+++ etc/do_algos.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 #
 TRAINBASE="reuters.martin.tok"
-TESTFILE="reuters.martin.tok.1000"
+TESTFILE="austen.l100"
 WOPR="../../wopr"
 SCRIPT="../../etc/do_algos.wopr_script"
 PXSCRIPT="../../etc/pplx_px.pl"
 LOG="README.ALG.txt"
 #
-CYCLE=0
+CYCLE=1000
 #
-date "+%Y-%m-%d %H:%M:%S" >> ${LOG}
+date "+#%Y-%m-%d %H:%M:%S" >> ${LOG}
 #
-for LINES in 9000 10000
+for LINES in 5000
 do
     #Make the dataset here, clean up afterwards?
     TRAINFILE=${TRAINBASE}.${LINES}
@@ -22,24 +22,37 @@ do
 	echo "Creating data file ${TRAINFILE}."
 	head -n ${LINES} ${TRAINBASE} > ${TRAINFILE}
     fi
-    for TIMBL in "-a1 +D" "-a4 +D"
+    for TIMBL in "-a1 +D" "-a1 +D -w0" "-a4 +D" "-a4 +D -w0" "-a4 +D -q1"
     do
 	for LC in 2 3
 	do
-	    for RC in 0 1 2
+	    for RC in 0 
 	    do
-		CYCLESTR=`printf "%03d" ${CYCLE}`
+		CYCLESTR=`printf "%04d" ${CYCLE}`
 		ID=ALG${CYCLESTR}
 		echo ${ID}
-		echo "${ID},${TRAINFILE},timbl:'${TIMBL}',lc:${LC},rc:${RC}" >> ${LOG}
+		echo "#${ID},${TRAINFILE},timbl:'${TIMBL}',lc:${LC},rc:${RC}" >> ${LOG}
 		echo ${WOPR} -s ${SCRIPT} -p trainfile:${TRAINFILE},rc:${RC},lc:${LC},id:${ID},timbl:${TIMBL},testfile:${TESTFILE}
 		${WOPR} -s ${SCRIPT} -p trainset:${TRAINFILE},rc:${RC},lc:${LC},id:${ID},timbl:"${TIMBL}",testset:${TESTFILE}
 		CYCLE=$(( $CYCLE + 1 ))
 		#
 		#reuters.martin.tok.1000.l3r2_ALG023.px
 		PXFILE=${TESTFILE}.l${LC}r${RC}_${ID}.px
-		echo ${PXFILE}
-		perl ${PXSCRIPT} -f ${PXFILE} -l ${LC} -r ${RC} | tail -n3
+		#printf -v S "%s %s" ${ID} ${PXFILE}
+		#echo $S
+		mrr_cd=0
+		mrr_cg=0
+		mrr_gd=0
+		STR=`perl ${PXSCRIPT} -f ${PXFILE} -l ${LC} -r ${RC} | tail -n3`
+		RX='RR\(cd\).* MRR: (.*).*RR\(cg\).* MRR: (.*).*RR\(gd\).* MRR: (.*)'
+		if [[ "${STR}" =~ $RX ]]
+		then
+		    mrr_cd=${BASH_REMATCH[1]}
+		    mrr_cg=${BASH_REMATCH[2]}
+		    mrr_gd=${BASH_REMATCH[3]}
+		fi
+		printf -v S "%s %s %s %s %s" ${ID} ${LINES}${mrr_cd} ${mrr_cg} ${mrr_gd}
+		echo ${S} >> ${LOG}
 	    done
 	done
     done
