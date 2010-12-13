2a406b32730516df217777a16436a26474fb74f2
pberck <pberck@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-12-13 12:09:16 +0000

    Script additions
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/wopr@7347 12f355fe-0486-481a-ad91-c297ab22b4e3

--- etc/create_algo_plots.sh
+++ etc/create_algo_plots.sh
@@ -28,6 +28,26 @@ done
 #   2) One context size, over the different algoritms.
 # And then there are the different measures to plot...
 #
+XR="[1000:]"
+YR="[]"
+#${ID} ${LINES} cg cd ic pplx pplx1 mrr_cd mrr_cg mrr_gd 
+#  1     2      3  4  5  6    7     8      9      10
+U="using 2:3"
+#
+VAR="pplx"
+IDX=1
+for TMP in "id" "LINES" "cg" "cd" "ic" "pplx" "pplx1" "mrr_cd" "mrr_cg" "mrr_gd"
+do
+    if [[ $VAR == $TMP ]]
+    then
+	echo $IDX
+	break
+    else
+	IDX=$(( $IDX + 1 ))
+    fi
+done 
+U="using 2:${IDX}"
+#
 # First one, plot file for each algortihm, to compare
 # scores for different contexts.
 #
@@ -40,12 +60,13 @@ do
 
     echo "set title \"${TSTR}\"" >>${GNUPLOT}
     echo "set xlabel \"instances\""  >>${GNUPLOT}
+    echo "set key bottom"  >>${GNUPLOT}
     echo "set logscale x" >>${GNUPLOT}
-    echo "set ylabel \"Foo\""  >>${GNUPLOT}
+    echo "set ylabel \"${VAR}\""  >>${GNUPLOT}
     echo "set grid"  >>${GNUPLOT}
     #set xtics (100,1000,10000,20000,100000) 
 
-    PLOT="plot [1000:][] "
+    PLOT="plot ${XR}${YR} "
     for LC in 1 2 3
     do
 	for RC in 0 1 2 3
@@ -53,7 +74,7 @@ do
 	    PLOTDATA=exp_l${LC}r${RC}_${TSTR}.data
 	    #echo "${PLOTDATA} using 2:10 w lp"
 	    # The 2:10 here is the MRR
-	    PLOT="${PLOT}\"${PLOTDATA}\" using 2:10 w lp,"
+	    PLOT="${PLOT}\"${PLOTDATA}\" ${U} w lp t \"l${LC}r${RC}\","
 	done
     done
     echo ${PLOT%\,}  >>${GNUPLOT}
@@ -71,24 +92,22 @@ do
 	echo "Generating ${GNUPLOT}"
 	echo "# autogen" >${GNUPLOT}
 	
-	echo "set title \"${TSTR}\"" >>${GNUPLOT}
+	echo "set title \"l${LC}r${RC}\"" >>${GNUPLOT}
 	echo "set xlabel \"instances\""  >>${GNUPLOT}
 	echo "set logscale x" >>${GNUPLOT}
-	echo "set ylabel \"Foo\""  >>${GNUPLOT}
+	echo "set ylabel \"${VAR}\""  >>${GNUPLOT}
 	echo "set grid"  >>${GNUPLOT}
 	#set xtics (100,1000,10000,20000,100000) 
 	
-	PLOT="plot [1000:][] "
+	PLOT="plot ${XR}${YR} "
 
 	for TIMBL in "-a1 +D" "-a4 +D" "-a4 +D -q1"
 	do
 	    TSTR="${TIMBL// /}"
 	    PLOTDATA=exp_l${LC}r${RC}_${TSTR}.data
 	    # The 2:10 here is the MRR
-	    PLOT="${PLOT}\"${PLOTDATA}\" using 2:10 w lp,"
+	    PLOT="${PLOT}\"${PLOTDATA}\" ${U} w lp t \"${TSTR}\","
 	done
 	echo ${PLOT%\,}  >>${GNUPLOT}
     done
 done
-	
-	
