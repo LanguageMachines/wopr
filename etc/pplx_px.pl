1bcab77e98d0918ab0e05387fdce40d7a92141c8
pberck <pberck@12f355fe-0486-481a-ad91-c297ab22b4e3>
2011-02-17 12:53:26 +0000

    -w option for word scores added.
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/wopr@8365 12f355fe-0486-481a-ad91-c297ab22b4e3

--- etc/pplx_px.pl
+++ etc/pplx_px.pl
@@ -9,14 +9,15 @@ use Getopt::Std;
 # ...
 #------------------------------------------------------------------------------
 
-use vars qw/ $opt_f $opt_i $opt_l $opt_r /;
+use vars qw/ $opt_f $opt_i $opt_l $opt_r $opt_w /;
 
-getopts('f:s:il:r:');
+getopts('f:il:r:w');
 
 my $wopr_file  = $opt_f || 0;
 my $ignore_oov = $opt_i || 0;
 my $lc         = $opt_l || 0;
 my $rc         = $opt_r || 0;
+my $wordscore  = $opt_w || 0;
 
 #------------------------------------------------------------------------------
 
@@ -26,13 +27,15 @@ for ( my $i = 0; $i < 6; $i++ ) {
   $vsum[$i] = 0;
 }
 my @vsum_txt = ( "unk", "match leaf", "cg", "cd", "ig" );
+my %word_score;			#cg,cd,ic counts
 my $log2prob_pos  = $lc + $rc + 2;
 my $target_pos    = $lc + $rc + 0;
+my $class_pos     = $lc + $rc + 1; #classification
 my $classtype_pos = $lc + $rc + 5;
 my $unknown_pos   = $lc + $rc + 6;
-my $md_pos        = $lc + $rc + 7; #match depth
-my $ml_pos        = $lc + $rc + 8; #matched at leaf
-my $dcnt_pos      = $lc + $rc + 9; #dist count
+my $md_pos        = $lc + $rc + 7;  #match depth
+my $ml_pos        = $lc + $rc + 8;  #matched at leaf
+my $dcnt_pos      = $lc + $rc + 9;  #dist count
 my $dsum_pos      = $lc + $rc + 10; #dist sum (of freqs in dist)
 my $rr_pos        = $lc + $rc + 11; #matched at leaf
 
@@ -46,7 +49,7 @@ my $dsum_sum = 0;
 my %rr_sum;
 my %rr_count;
 
-my $f = "%05b"; # Number of binary indicators
+my $f = "%05b";			# Number of binary indicators
 
 open(FHW, $wopr_file)  || die "Can't open file.";
 
@@ -55,77 +58,80 @@ while ( my $line = <FHW> ) {
   if ( substr($line, 0, 1) eq "#" ) {
     next;
   }
-    chomp $line;
+  chomp $line;
 
-    my @parts = [];
-    my $target;
-    my $wopr_log2prob;
-    my $wopr_log10prob;
-    my $wopr_prob;
-    my $classtype;
-    my $icu;
-    my $dcnt;
-    my $dsum;
-    my $md;
-    my $ml;
-    my $extra = 0;
-    my $indicators = 0;
-    my $rr;
+  my @parts = [];
+  my $target;
+  my $wopr_log2prob;
+  my $wopr_log10prob;
+  my $wopr_prob;
+  my $classtype;
+  my $icu;
+  my $dcnt;
+  my $dsum;
+  my $md;
+  my $ml;
+  my $extra = 0;
+  my $indicators = 0;
+  my $rr;
     
-    @parts  = split (/ /, $line);
-    $target         = $parts[ $target_pos ];
-    $wopr_log2prob  = $parts[ $log2prob_pos ]; # native logp value
-    $wopr_prob      = 2 ** $wopr_log2prob;
-    $wopr_log10prob = log2tolog10( $wopr_log2prob );
-    $icu            = $parts[$unknown_pos];
-    $md             = $parts[$md_pos];
-    $ml             = $parts[$ml_pos];
-    $classtype      = $parts[$classtype_pos];
-    $rr             = $parts[$rr_pos];
-    $dcnt           = $parts[$dcnt_pos];
-    $dsum           = $parts[$dsum_pos];
+  @parts  = split (/ /, $line);
+  $target         = $parts[ $target_pos ];
+  $wopr_log2prob  = $parts[ $log2prob_pos ]; # native logp value
+  $wopr_prob      = 2 ** $wopr_log2prob;
+  $wopr_log10prob = log2tolog10( $wopr_log2prob );
+  $icu            = $parts[$unknown_pos];
+  $md             = $parts[$md_pos];
+  $ml             = $parts[$ml_pos];
+  $classtype      = $parts[$classtype_pos];
+  $rr             = $parts[$rr_pos];
+  $dcnt           = $parts[$dcnt_pos];
+  $dsum           = $parts[$dsum_pos];
 
-    if ( $ignore_oov ) {
-      $icu = "k";
-    }
-    if ( $target eq "<\/s>" ) {
-      ++$sentencecount;
-    } else {
-      ++$wordcount;
-    }
-    if ( $icu eq "u" ) {
-      $indicators += 1;
-      ++$oovcount;
-      $vsum[0]++;
-    }
-    if ( $ml == 1 ) {
-      $indicators += 2;
-      $vsum[1]++;
-    }
-    if ( $classtype eq "cg" ) {
-      $indicators += 4;
-      $vsum[2]++;
-    } elsif ( $classtype eq "cd" ) {
-      $indicators += 8;
-      $vsum[3]++;
-    } elsif ( $classtype eq "ic" ) {
-      $indicators += 16;
-      $vsum[4]++;
-    }
-    if ( ! ($classtype eq "ic") ) {    
-	$rr_sum{$classtype} += $rr;
-	++$rr_count{$classtype};
-	$rr_sum{"gd"} += $rr;
-	++$rr_count{"gd"};	
-    }
+  if ( $ignore_oov ) {
+    $icu = "k";
+  }
+  if ( $target eq "<\/s>" ) {
+    ++$sentencecount;
+  } else {
+    ++$wordcount;
+  }
+  if ( $icu eq "u" ) {
+    $indicators += 1;
+    ++$oovcount;
+    $vsum[0]++;
+  }
+  if ( $ml == 1 ) {
+    $indicators += 2;
+    $vsum[1]++;
+  }
+  if ( $classtype eq "cg" ) {
+    $indicators += 4;
+    $vsum[2]++;
+  } elsif ( $classtype eq "cd" ) {
+    $indicators += 8;
+    $vsum[3]++;
+  } elsif ( $classtype eq "ic" ) {
+    $indicators += 16;
+    $vsum[4]++;
+  }
+  if ( ! ($classtype eq "ic") ) {    
+    $rr_sum{$classtype} += $rr;
+    ++$rr_count{$classtype};
+    $rr_sum{"gd"} += $rr;
+    ++$rr_count{"gd"};	
+  }
 
-    $dcnt_sum += $dcnt;
-    $dsum_sum += $dsum;
+  $word_score{$target}{$classtype} += 1;
 
-    if ( ($icu eq "k") && ($wopr_log10prob != 0) ) {
-      $wopr_sumlog10 += $wopr_log10prob;
-    }
+  $dcnt_sum += $dcnt;
+  $dsum_sum += $dsum;
 
+  if ( ($icu eq "k") && ($wopr_log10prob != 0) ) {
+    $wopr_sumlog10 += $wopr_log10prob;
+  }
+
+  if ( $wordscore == 0 ) {
     printf( "%.8f %8.4f %8.4f ", $wopr_prob, $wopr_log2prob, $wopr_log10prob );
     printf( "%2i %1i ", $md, $ml );
 
@@ -134,48 +140,68 @@ while ( my $line = <FHW> ) {
     print "$target";
     print "\n";
     ++$summary{$indicators};
-  } 
+  }
+} 
 
-my $tot = 0;
-foreach my $key (sort (keys(%summary))) {
-  $tot += $summary{$key};
-}
+close(FHW);
 
-foreach my $key (sort { $a <=> $b } (keys(%summary))) {
-  printf( "$f:%6i (%6.2f%%)\n", $key, $summary{$key},  $summary{$key}*100/$tot );
-}
+if ( $wordscore == 0 ) {
+  my $tot = 0;
+  foreach my $key (sort (keys(%summary))) {
+    $tot += $summary{$key};
+  }
 
-printf ("dist_freq sum: %6i, ave: (%6.2f)\n", $dcnt_sum, $dcnt_sum/$tot);
-printf ("dist_sum sum: %6i, ave: (%6.2f)\n", $dsum_sum, $dsum_sum/$tot);
+  foreach my $key (sort { $a <=> $b } (keys(%summary))) {
+    printf( "$f:%6i (%6.2f%%)\n", $key, $summary{$key},  $summary{$key}*100/$tot );
+  }
 
-for ( my $i = 0; $i < 5; $i++ ) {
-  my $frmt = "%".($i+1)."i";
-  if ( $tot != 0 ) {
-    printf( "Column: %2i %6i (%6.2f%%) %s\n", $i, $vsum[$i],  $vsum[$i]*100/$tot, $vsum_txt[$i] );
+  printf ("dist_freq sum: %6i, ave: (%6.2f)\n", $dcnt_sum, $dcnt_sum/$tot);
+  printf ("dist_sum sum: %6i, ave: (%6.2f)\n", $dsum_sum, $dsum_sum/$tot);
+
+  for ( my $i = 0; $i < 5; $i++ ) {
+    my $frmt = "%".($i+1)."i";
+    if ( $tot != 0 ) {
+      printf( "Column: %2i %6i (%6.2f%%) %s\n", $i, $vsum[$i],  $vsum[$i]*100/$tot, $vsum_txt[$i] );
+    }
+  }
+  printf( "Total: %6i\n", $tot );
+
+  print "\n";
+  print "Sum log10 probs:\n";
+  printf( "Wopr:  %8.2f\n", $wopr_sumlog10 );
+  printf( "Wordcount: %i sentencecount: %i oovcount: %i\n", 
+	  $wordcount, $sentencecount, $oovcount );
+  if ( ! $ignore_oov ) {
+    printf( "Wopr ppl:  %8.2f ", 10 ** (-$wopr_sumlog10/($wordcount-$oovcount+$sentencecount)));
+    printf( "Wopr ppl1:  %8.2f ", 10 ** (-$wopr_sumlog10/($wordcount-$oovcount)));
+    print " (No oov words.)\n";
+  } else {
+    printf( "Wopr ppl:  %8.2f ", 10 ** (-$wopr_sumlog10/($wordcount+$sentencecount)));
+    printf( "Wopr ppl1:  %8.2f ", 10 ** (-$wopr_sumlog10/($wordcount)));
+    print " (With oov words.)\n";
+  }
+
+  print "\n";
+  foreach my $key (sort (keys(%rr_sum))) {
+    printf( "RR(%s): %8.3f, MRR: %8.3f\n", $key, $rr_sum{$key}, $rr_sum{$key}/$rr_count{$key});
   }
 }
-printf( "Total: %6i\n", $tot );
-
-print "\n";
-print "Sum log10 probs:\n";
-printf( "Wopr:  %8.2f\n", $wopr_sumlog10 );
-printf( "Wordcount: %i sentencecount: %i oovcount: %i\n", 
-	$wordcount, $sentencecount, $oovcount );
-if ( ! $ignore_oov ) {
-  printf( "Wopr ppl:  %8.2f ", 10 ** (-$wopr_sumlog10/($wordcount-$oovcount+$sentencecount)));
-  printf( "Wopr ppl1:  %8.2f ", 10 ** (-$wopr_sumlog10/($wordcount-$oovcount)));
-  print " (No oov words.)\n";
-} else  {
-  printf( "Wopr ppl:  %8.2f ", 10 ** (-$wopr_sumlog10/($wordcount+$sentencecount)));
-  printf( "Wopr ppl1:  %8.2f ", 10 ** (-$wopr_sumlog10/($wordcount)));
-  print " (With oov words.)\n";
+if ( $wordscore == 1 ) {
+  foreach my $key (sort (keys(%word_score))) {
+    #check keys
+    unless ( defined $word_score{$key}{"cg"} ) {
+      $word_score{$key}{"cg"} = 0;
+    }
+    unless ( defined $word_score{$key}{"cd"} ) {
+      $word_score{$key}{"cd"} = 0;
+    }
+    unless ( defined $word_score{$key}{"ic"} ) {
+      $word_score{$key}{"ic"} = 0;
+    }
+    printf( "%s: cg:%s cd:%s ic:%s\n", $key, $word_score{$key}{"cg"}, $word_score{$key}{"cd"}, $word_score{$key}{"ic"});
+  }
 }
 
-print "\n";
-foreach my $key (sort (keys(%rr_sum))) {
-    printf( "RR(%s): %8.3f, MRR: %8.3f\n", $key, $rr_sum{$key}, $rr_sum{$key}/$rr_count{$key});
-}
-close(FHW);
 
 # NB, perl log is base e.
 #
@@ -193,5 +219,5 @@ px output:
 
 # instance+target classification logprob entropy word_lp guess (dist.cnt [topn])
 _ _ The `` -3.34807 8.24174 10.1829 cd 1 0 9103 [ `` 14787 The 10598 But 4149  ]
-_ The Plastics Derby -21.1142 10.5035 2.26994e+06 icu 2 1 3236 [ Derby 152 New 74 ]
+  _ The Plastics Derby -21.1142 10.5035 2.26994e+06 icu 2 1 3236 [ Derby 152 New 74 ]
 
