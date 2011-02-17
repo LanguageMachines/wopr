fa1b31f16b2da1c590aa642dba58fed041b8d74d
pberck <pberck@12f355fe-0486-481a-ad91-c297ab22b4e3>
2011-02-17 14:24:08 +0000

    File added.
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/wopr@8368 12f355fe-0486-481a-ad91-c297ab22b4e3

---
+++ etc/create_cg_f_scatterplot.pl
@@ -0,0 +1,84 @@
+#!/usr/bin/perl -w
+# $Id: $
+#
+use strict;
+use Getopt::Std;
+
+#------------------------------------------------------------------------------
+# Takes a -w output file from pplx_px.pl and a (corresponding) 
+# lexicon file. Creates a gnuplot file to plot a scatter graph
+# for correct-guess-percentage against frequency.
+#------------------------------------------------------------------------------
+
+use vars qw/ $opt_w $opt_l /;
+
+getopts('w:l:');
+
+my $ws_file  = $opt_w || 0; #file from "pplx_pl.pl -w"
+my $lex_file = $opt_l || 0; #lexicon file, lines of: word freq
+
+#------------------------------------------------------------------------------
+
+my %lex;
+
+open(FHW, $lex_file)  || die "Can't open file.";
+while ( my $line = <FHW> ) {
+  chomp $line;
+
+  my @parts  = split(/ /, $line);
+
+  $lex{$parts[0]} = $parts[1];
+}
+close(FHW);
+
+open(FHW, $ws_file)  || die "Can't open file.";
+while ( my $line = <FHW> ) {
+  if ( substr($line, 0, 1) eq "#" ) {
+    next;
+  }
+  chomp $line;
+
+  if ( $line =~ /(.*?): cg:(.*?) \((.*?)%\) cd:(.*?) \((.*?)%\) ic:(.*?) \((.*?)%\)/ ) {
+
+    my $word = $1;
+    my $cg_a = $2; #_absolute
+    my $cg_p = $3; #_percentage
+    my $cd_a = $4;
+    my $cd_p = $5;
+    my $ic_a = $6;
+    my $ic_p = $7;
+
+    my $lex_f = $lex{$word} || 0;
+    if ( $lex_f ) {
+      print "$word $cg_p $cd_p $ic_p $lex_f\n";
+    }
+  }
+  
+}
+close(FHW);
+
+# NB, perl log is base e.
+#
+sub log2tolog10 {
+  my $l    = shift;
+  my $e    = 2.718281828;
+  my $log2 = 0.3010299957;
+
+  return $l * $log2;
+}
+
+__END__
+
+ws file:
+with: cg:18 (15.25%) cd:73 (61.86%) ic:27 (22.88%)
+
+set title "scatter"
+set xlabel "frequency"
+set key bottom
+set ylabel "cg"
+set grid
+plot [][] "foo.data" using 5:2
+
+
+foo.data:
+Chinese 66.67 33.33 0.00 226
