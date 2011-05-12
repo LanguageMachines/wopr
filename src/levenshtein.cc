1b139d758b75eb5812b43ed22b4caaf7be410617
pberck <pberck@12f355fe-0486-481a-ad91-c297ab22b4e3>
2011-05-12 11:11:20 +0000

    Missing includes
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/wopr@9890 12f355fe-0486-481a-ad91-c297ab22b4e3

--- src/levenshtein.cc
+++ src/levenshtein.cc
@@ -39,6 +39,16 @@
 #include <math.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
 
 #include "qlog.h"
 #include "util.h"
