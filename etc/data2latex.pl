223687d9e1ec10fc23aefb31c0ec19f2a9a423de
pberck <pberck@12f355fe-0486-481a-ad91-c297ab22b4e3>
2011-08-02 07:00:43 +0000

    Fixes in script.
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/wopr@12773 12f355fe-0486-481a-ad91-c297ab22b4e3

--- etc/data2latex.pl
+++ etc/data2latex.pl
@@ -7,11 +7,12 @@ use Getopt::Std;
 # User options
 #------------------------------------------------------------------------------
 
-use vars qw/ $opt_f /;
+use vars qw/ $opt_f $opt_v /;
 
-getopts('f:');
+getopts('f:v:');
 
-my $file       = $opt_f ||  0;
+my $file       = $opt_f || 0;
+my $vars       = $opt_v || "cg";
 
 open(FH, $file) || die "Can't open file.";
 
@@ -84,23 +85,29 @@ while ( my $line = <FH> ) {
     print "& \\num{".sprintf( "%.1f", $parts[$cg])."} ";     #cg
     print "& \\num{".sprintf( "%.1f", $parts[$cg+1])."} ";   #cd
     print "& \\num{".sprintf( "%.1f", $parts[$cg+2])."} ";   #ic
-    print "& \\num{".sprintf( "%i", $parts[$cg+3])."} ";      #gcs
+    print "& \\num{".sprintf( "%i", $parts[$cg+3])."} ";     #gcs
     print "& \\num{".sprintf( "%i", $parts[$cg+4])."} ";     #gcd
     print "& \\cmp{".sprintf( "%s", $parts[$cg+6])."} ";     #range
     if ( $#parts > 10 ) {
       print "& \\num{".sprintf( "%.0f", $parts[$cg+8])."} ";   #pplx
-      print "& \\num{".sprintf( "%.3f", $parts[$cg+10])."} ";    #mmr(cd)
+      print "& \\num{".sprintf( "%.3f", $parts[$cg+10])."} ";  #mmr(cd)
     } else {
       print "& \\num{0} ";   #pplx
-      print "& \\num{0} ";    #mmr(cd)
+      print "& \\num{0} ";   #mmr(cd)
     }
   }
-  if ( $cg == 2 ) { 
-    print "& \\num{".sprintf( "%.1f", $parts[$cg])."} ";     #cg
-    print "& \\num{".sprintf( "%.1f", $parts[$cg+1])."} ";   #cd
-    print "& \\num{".sprintf( "%.1f", $parts[$cg+2])."} ";   #ic
-    print "& \\num{".sprintf( "%.0f", $parts[$cg+3])."} ";   #pplx
-    print "& \\num{".sprintf( "%.3f", $parts[$cg+5])."} ";   #mmr(cd)
+  if ( $cg == 2 ) { # ALG
+    if ( $vars eq "cg" ) {
+      print "& \\num{".sprintf( "%.1f", $parts[$cg])."} ";     #cg
+      print "& \\num{".sprintf( "%.1f", $parts[$cg+1])."} ";   #cd
+      print "& \\num{".sprintf( "%.1f", $parts[$cg+2])."} ";   #ic
+      print "& \\num{".sprintf( "%.0f", $parts[$cg+3])."} ";   #pplx
+      print "& \\num{".sprintf( "%.3f", $parts[$cg+5])."} ";   #mmr(cd)
+    }
+    if ( $vars eq "ads" ) {
+      print "& \\num{".sprintf( "%.0f", $parts[$cg+8])."} ";   #adc
+      print "& \\num{".sprintf( "%.0f", $parts[$cg+9])."} ";   #ads(umfreq)
+    }
     (my $timbl = $parts[$cg+10]) =~ tr/_/ /;
     print "& \\cmp{".sprintf( "%s", $timbl)."} ";  #timbl
   }
