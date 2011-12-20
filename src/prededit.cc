ae05627f6a517a82899fae591381a0b689b35d9a
pberck <pberck@12f355fe-0486-481a-ad91-c297ab22b4e3>
2011-12-20 12:23:54 +0000

    Work on pdt2.
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/wopr@13729 12f355fe-0486-481a-ad91-c297ab22b4e3

--- src/prededit.cc
+++ src/prededit.cc
@@ -99,6 +99,7 @@ struct salad_elem {
 };
 void generate_next( Timbl::TimblAPI*, Config&, std::string, std::vector<distr_elem>& );
 void generate_tree( Timbl::TimblAPI*, Config&, Context&, std::vector<std::string>&, int, std::vector<int>&, std::string );
+int explode(std::string, std::vector<std::string>&);
 void window_word_letters(std::string, std::string, int, Context&, std::vector<std::string>&);
 void window_words_letters(std::string, int, Context&, std::vector<std::string>&);
 
@@ -154,6 +155,9 @@ int pdt( Logfile& l, Config& c ) {
   l.log( "mo:         "+to_str(matchesonly) );
   l.log( "OUTPUT:     "+output_filename );
   l.dec_prefix();
+
+
+  
 
   try {
     My_Experiment = new Timbl::TimblAPI( timbl );
@@ -409,6 +413,7 @@ int pdt2( Logfile& l, Config& c ) {
   int                rc1             = stoi( c.get_value( "rc1", "0" )); // should be 0
   std::string        ped             = c.get_value( "ds", "" ); // depths
   int                pel             = stoi( c.get_value( "n", "3" )); // length
+  std::string        dl              = c.get_value( "dl", "3" ); // letter depth 
   bool               matchesonly     = stoi( c.get_value( "mo", "0" )) == 1; // show only matches
   std::string        id              = c.get_value( "id", to_str(getpid()) );
 
@@ -425,7 +430,7 @@ int pdt2( Logfile& l, Config& c ) {
   }
 
   int length0 = 1; // hardcoded
-  std::string ped0 = "5"; // hardcoded
+  std::string ped0 = dl;
   std::vector<int> depths0(length0+1, 1);
   for( int i = 0; i < ped0.size(); i++) {
     depths0.at(length0-i) = stoi( ped0.substr(i, 1), 32 ); // V=31
@@ -440,7 +445,8 @@ int pdt2( Logfile& l, Config& c ) {
     depths.at(length-i) = stoi( ped.substr(i, 1), 32 ); // V=31
   }
 
-  std::string tmp = "n"+to_str(length)+"ds";
+  std::string tmp = "dl"+dl;
+  tmp = tmp + "_n"+to_str(length)+"ds";
   for ( int i = length; i > 0; i-- ) {
     if ( (length-i) < ped.size() ) {
       tmp = tmp + ped[length-i];
@@ -464,6 +470,26 @@ int pdt2( Logfile& l, Config& c ) {
   l.log( "mo:         "+to_str(matchesonly) );
   l.log( "OUTPUT:     "+output_filename );
   l.dec_prefix();
+
+  // TEST
+  //
+  /*
+  int test_lc = 8;
+  Context test_ctx(test_lc);
+  std::string test_word = "Hoewel de politie .";
+  std::vector<std::string> test_words;
+  Tokenize( test_word, test_words );
+  std::vector<std::string> test_res;
+  std::vector<std::string>::iterator tri;
+  window_words_letters(test_word, test_lc, test_ctx, test_res);
+  for ( tri = test_res.begin(); tri != test_res.end(); tri++ ) {
+    l.log(*tri);
+  }
+  return 0;
+  */
+  //
+  // END TEST
+
 
   try {
     My_Experiment0 = new Timbl::TimblAPI( timbl0 );
@@ -506,6 +532,7 @@ int pdt2( Logfile& l, Config& c ) {
 
   std::string a_line;
   std::string token;
+  std::string letter;
   std::string result;
   std::vector<std::string> results;
   std::vector<std::string> targets;
@@ -513,7 +540,9 @@ int pdt2( Logfile& l, Config& c ) {
   const Timbl::ValueDistribution *vd;
   const Timbl::TargetValue *tv;
   std::vector<std::string> words;
+  std::vector<std::string> letters;
   std::vector<std::string>::iterator wi;
+  std::vector<std::string>::iterator li;
   std::vector<std::string> predictions;
   std::vector<std::string>::iterator pi;
   int correct = 0;
@@ -526,34 +555,24 @@ int pdt2( Logfile& l, Config& c ) {
 
   MTRand mtrand;
 
-  // TEST
+  // Letter context, from ibase0
   //
-  int test_lc = 8;
-  Context test_ctx(test_lc);
-  std::string test_word = "Hoewel de politie en";
-  std::vector<std::string> test_res;
-  window_words_letters(test_word, test_lc, test_ctx, test_res);
-  return 0;
-  //
-  // END TEST
-
-  std::string ectx0; // empty context
-  for ( int i = 0; i < lc0+rc1; i++ ) {
-    ectx0 = ectx0 + "_ ";
-  }
-
   int ctx_size0 = lc0+rc0;
   Context ctx0(ctx_size0);
-  std::vector<distr_elem> res;
 
-  //std::vector<std::string>::iterator it_endm1 = ctx.begin();
-  //advance(it_end,4);
+  // Word context, from ibase1
+  //
+  int ctx_size1 = lc1+rc1;
+  Context ctx1(ctx_size1);
+
+  std::vector<distr_elem> res;
 
   file_out << "# l" << length;
   for ( int i = length; i > 0; i-- ) {
     file_out << " " << depths.at(i); // choices per 'column'
   }
-  file_out << " " << ibasefile0 << " " << ibasefile1;
+  file_out << std::endl;
+  file_out << "# " << ibasefile0 << " " << ibasefile1;
   file_out << std::endl;
 
   size_t sentence_count = 0;
@@ -563,6 +582,11 @@ int pdt2( Logfile& l, Config& c ) {
   int skip = 0;
   long keypresses = 0;
   long keyssaved = 0;
+  long letterssaved = 0;
+
+
+
+
 
   while( std::getline( file_in, a_line ) ) { 
     if ( a_line == "" ) {
@@ -586,6 +610,7 @@ int pdt2( Logfile& l, Config& c ) {
     //
     long sentenceksaved = 0; // key presses saved in this sentence
     long sentencewsaved = 0; // words saved in this sentence
+
     for ( int i = 0; i < words.size(); i++ ) {
 
       token = words.at(i);
@@ -593,42 +618,119 @@ int pdt2( Logfile& l, Config& c ) {
       // We got word 'token' (pretend we just pressed space).
       // Add to context, and call wopr.
       //
-      ctx0.push( token );
+      ctx1.push( token );
 
       if ( skip > 0 ) {
 	//l.log( "Skipping: " + to_str(skip) );
 	// increment instance count? Log? E, edited, excluded
+	//
 	file_out << "E" << std::setfill('0') << std::setw(4) << sentence_count << "." 
 		 << std::setfill('0') << std::setw(4) << instance_count << " "; 
-	file_out << ctx0 << std::endl;
+	file_out << ctx1 << std::endl;
 	++instance_count;
 	--skip;
+
+	// We need to skip the letter context as well now.
+	//
+	(void)explode( token, letters );
+	for ( int j = 0; j < letters.size(); j++ ) {
+	  letter = letters.at(j);
+	  ctx0.push( letter );
+	}
+	ctx0.push( "_" );
+
 	continue;
       }
 
-      // _ t the
-      // t h the
-      // h e the
-      //
       std::vector<std::string> strs; // should be a struct with more stuff
-      std::vector<std::string>::iterator si;
       std::string t;
-      generate_tree( My_Experiment0, c, ctx0, strs, length0, depths0, t );
 
-      for ( si = strs.begin(); si != strs.end(); si++ ) {
-	l.log( *si );
-      }
+      // Explode word into letters.
+      // Loop over all letters here, do predictions, just to get
+      // starting context for the ibase1 classifier, which continues word based.
+      // We need a data struct here to save data in this loop.
+      //
+      letters.clear();
+      (void)explode( token, letters );
+      int lsaved = 0;
+      //l.log( "Inside "+token+", letters="+to_str(letters.size()) );
+      for ( int j = 0; j < letters.size(); j++ ) {
+
+	// get the letter we "typed", and add it to the context used
+	// to predict the rest of the word.
+	//
+	letter = letters.at(j);
+	ctx0.push( letter );
+
+	// Predict 5 words we could be typing at the moment.
+	// From these 5, we predict more words with ibase1.
+	//
+	std::vector<std::string> strs0;
+	std::string t0;
+
+	generate_tree( My_Experiment0, c, ctx0, strs0, length0, depths0, t0 );
+
+	for ( int s = 0; s < strs0.size(); s++ ) {
+	  std::string lpred = strs0.at(s).substr(1, strs0.at(s).length()-1);
+	  //l.log( "pred="+to_str(s)+"/"+lpred );
+	  
+	  if ( lpred == token ) { //NB these spaces in the beginning
+	    //l.log( "MATCH INSIDE WORD" );
       
-      // Now generate from My_Experiment1
+	    lsaved = letters.size()-j; // NB, this includes the space after the word.
+
+	    file_out << "L" << std::setfill('0') << std::setw(4) << sentence_count << "." 
+		     << std::setfill('0') << std::setw(4) << instance_count << " "; 
+	    file_out << ctx0 << " " << lpred << " " << j << "/" << letters.size() << "=" << lsaved << std::endl;
+
+	    // The pred. should be insterted in this context here, if match inside word.
+	    // actually, the lpred is what is already in ctx1, because if we match it is
+	    // the current word.
+	    // We "just" need to know how many extra letters we saved
+	    //
+	    Context ctx01 = ctx1;
+	    //l.log( "ctx1="+ctx1.toString() );
+	    //l.log( "ctx01="+ctx01.toString() );
+
+	    generate_tree( My_Experiment1, c, ctx01, strs, length, depths, t );
+
+	    // Break? We only need to predict our word once.
+	    // break outerloop also
+	    //
+	    break;
+	  }
+	  if ( lsaved > 0 ) {
+	    break;
+	  }
+	} // s
+	if ( lsaved > 0 ) {
+	  break;
+	}
+      } //letters
+      // End of word.
+      letters.clear();
+      ctx0.push( "_" );
+
+      //l.log( "IBASE1 predictions: "+to_str(strs.size()) );
+
+      // If lsaves == 0, we call 
+      // generate_tree( My_Experiment1, c, ctx01, strs, length, depths, t );
+      // again?
+      if ( lsaved == 0 ) {
+	//l.log( "Adding predictions." );
+	//l.log( "ctx1="+ctx1.toString() );
+	generate_tree( My_Experiment1, c, ctx1, strs, length, depths, t );
+	//l.log( "IBASE1 predictions: "+to_str(strs.size()) );
+      }
 
       // Print the instance, with counts.
       // I0000.0004 waited for
       //
       file_out << "I" << std::setfill('0') << std::setw(4) << sentence_count << "." 
 	       << std::setfill('0') << std::setw(4) << instance_count << " "; 
-      file_out << ctx0 << std::endl;
+      file_out << ctx1 << std::endl;
 
-      si = strs.begin();
+      std::vector<std::string>::iterator si = strs.begin();
       prediction_count = 0;
       long savedhere = 0; // key presses saved in this prediction.
       while ( si != strs.end() ) {
@@ -670,7 +772,7 @@ int pdt2( Logfile& l, Config& c ) {
 	if ( matched.size() > savedhere+1 ) {
 	  //if ( words_matched > skip ) {
 	  skip = words_matched;
-	  savedhere = matched.size()-1;
+	  savedhere = matched.size()-1; // PLUS lsaved?
 	  sentenceksaved += savedhere;
 	  sentencewsaved += words_matched;
 	}
@@ -678,6 +780,8 @@ int pdt2( Logfile& l, Config& c ) {
 	// P0000.0004.0004 his sake and
 	//
 	// (print only when a match, optional?)
+	//
+	// The lsaved can be added here, or counted seperately.
 	//
 	if ( ( matchesonly && (matched != "") ) || ( matchesonly == false ) ) {
 	file_out << "P" << std::setfill('0') << std::setw(4) << sentence_count << "." 
@@ -688,7 +792,7 @@ int pdt2( Logfile& l, Config& c ) {
 	  file_out << "M" << std::setfill('0') << std::setw(4) << sentence_count << "." 
 		   << std::setfill('0') << std::setw(4) << instance_count << "." 
 		   << std::setfill('0') << std::setw(4) << prediction_count 
-		   << " " << matched << matched.size()-1 << std::endl; // -1 for trailing space
+		   << " " << matched << matched.size()-1 << std::endl; // -1 for trailing space. 
 	}
 	}
 	prediction_count++;
@@ -697,6 +801,7 @@ int pdt2( Logfile& l, Config& c ) {
       strs.clear();
       
       keyssaved += savedhere;
+      letterssaved += lsaved;
       
       ++instance_count;
     } // i over words
@@ -704,22 +809,29 @@ int pdt2( Logfile& l, Config& c ) {
     // Output Result for this sentence.
     //
     file_out << "R" << std::setfill('0') << std::setw(4) << sentence_count << " "; 
-    file_out << /*a_line << " " <<*/ sentencewsaved << " " << sentenceksaved << std::endl;
+    file_out << /*a_line << " " <<*/ sentencewsaved << " " << sentenceksaved << " ";
+    file_out << letterssaved << std::endl;
 
-    ctx0.reset();
+    ctx1.reset();
 
     ++sentence_count;
   }
 
+
+
+
   // Output Totals
   //
-  file_out << "T " << keypresses << " " << keyssaved << " " << ((double)keyssaved/keypresses)*100.0 << std::endl;
+  file_out << "T0 " << keypresses << " " << letterssaved << " " << ((double)letterssaved/keypresses)*100.0 << std::endl;
+  file_out << "T1 " << keypresses << " " << keyssaved << " " << ((double)keyssaved/keypresses)*100.0 << std::endl;
+  file_out << "T " << keypresses << " " << (keyssaved+letterssaved) << " " << ((double)(keyssaved+letterssaved)/keypresses)*100.0 << std::endl;
 
   l.log( "Keypresses: " + to_str(keypresses) );
+  l.log( "Letters   : " + to_str(letterssaved) );
   l.log( "Saved     : " + to_str(keyssaved) );
 
-  c.add_kv( "pdt_file", output_filename );
-  l.log( "SET pdt_file to "+output_filename );
+  c.add_kv( "pdt2_file", output_filename );
+  l.log( "SET pdt2_file to "+output_filename );
 
   return 0;
 }  
@@ -781,10 +893,12 @@ void generate_tree( Timbl::TimblAPI* My_Experiment, Config& c, Context& ctx, std
 
   if ( length == 0 ) {
     // here we should save/print the whole "string" (?)
-    //std::cout << "STRING: " << t << std::endl;
+    //std::cout << "STRING: [" << t << "]" << std::endl;
     strs.push_back( t );
     return;
   }
+
+  //std::cerr << "gen_tree: ctx=" << ctx.toString() << " / l=" << length << ", t=" << t << std::endl;
 
   const Timbl::ValueDistribution *vd;
   const Timbl::TargetValue *tv;
@@ -813,13 +927,15 @@ void generate_tree( Timbl::TimblAPI* My_Experiment, Config& c, Context& ctx, std
   while ( (fi != res.end()) && (--cnt >= 0) ) { 
     
     //for ( int i = 5-length; i > 0; i--) { std::cout << "  ";}
-    //std::cerr << length << ":" << cnt  << " " << ctx << " -> " << (*fi).name /* << ' ' << (*fi).freq */ << std::endl;
+    // std::cerr << length << ":" << cnt  << " " << ctx << " -> " << (*fi).name /* << ' ' << (*fi).freq */ << std::endl;
 
     // Make new context, recurse
     //
     Context new_ctx = Context(ctx);
     new_ctx.push(  (*fi).name );
 
+    // NB the extra space in the beginning.
+    //
     generate_tree( My_Experiment, c, new_ctx, strs, length-1, depths, t+" "+(*fi).name );//+"/"+to_str(res.size()) );
     
     fi++;
@@ -827,17 +943,34 @@ void generate_tree( Timbl::TimblAPI* My_Experiment, Config& c, Context& ctx, std
   
 }
 
+// --
+
+// foo -> f o o
+//
+int explode(std::string a_word, std::vector<std::string>& res) {
+  for ( int i = 0; i < a_word.length(); i++ ) {
+    std::string tmp = a_word.substr(i, 1);
+    res.push_back( tmp );
+  }
+  return res.size();
+}
+
 // _ T The
 // T h The
-// h e The
+// h e The (or: h e _)
 //
 void window_word_letters(std::string a_word, std::string t, int lc, Context& ctx, std::vector<std::string>& res) {
   int i;
-  for ( i = 0; i < a_word.length(); i++ ) {
+  for ( i = 0; i < a_word.length()-1; i++ ) { //NB ()-1
     std::string tmp = a_word.substr(i, 1);
     ctx.push( tmp ); // next letter in context
     res.push_back( ctx.toString() + " " + t );
   }
+  // After the last letter, predict a space instead.
+  // Do we want to predict spaces?
+  std::string tmp = a_word.substr(i, 1);
+  ctx.push( tmp ); // next letter in context
+  res.push_back( ctx.toString() + " " + "_" ); //instead of t
 }
 
 // Loop over one sentence.
@@ -847,77 +980,13 @@ void window_words_letters(std::string a_line, int lc, Context& ctx, std::vector<
   std::vector<std::string>::iterator wi;
   Tokenize( a_line, words, ' ' );
   int i;
-  for ( i = 0; i < words.size()-1; i++ ) { // each word
-    window_word_letters( words.at(i), words.at(i), lc, ctx, res );
-    //
-    // After a word we get a space, and predict the _next_ word.
-    // (but not if we are the last word.)
-    //
-    window_word_letters( "_", words.at(i+1), lc, ctx, res );
-  }
-  if ( words.at(i).size() > 0 ) { //not if one character, bcz already in words.at(i+1)? 
+  for ( i = 0; i < words.size(); i++ ) { // each word
+    if ( i > 0 ) {
+      ctx.push( "_" );
+      res.push_back( ctx.toString() + " " + words.at(i)); 
+    }
     window_word_letters( words.at(i), words.at(i), lc, ctx, res );
   }
-}
-
-// Process whole corpus.
-// Do this not per getline() but with >>next_word ?
-//
-int window_letters_off( Logfile& l, Config& c ) {
-  l.log( "window_letters" );
-  const std::string& filename        = c.get_value( "filename" );
-  int                lc              = stoi( c.get_value( "lc", "2" ));
-  int                rc              = 0;
-  std::string        output_filename = filename + ".lt" + to_str(lc); // RC makes no sense (yet)
-
-  l.inc_prefix();
-  l.log( "filename:  "+filename );
-  l.log( "lc:        "+to_str(lc) );
-  l.log( "OUTPUT:    "+output_filename );
-  l.dec_prefix();
-
-  if ( file_exists( l, c, output_filename )) {
-    l.log( "OUTPUT exists, not overwriting." );
-    c.add_kv( "filename", output_filename );
-    l.log( "SET filename to "+output_filename );
-    return 0;
-  }
- 
-  std::ifstream file_in( filename.c_str() );
-  if ( ! file_in ) {
-    l.log( "ERROR: cannot load file." );
-    return -1;
-  }
-  std::ofstream file_out( output_filename.c_str(), std::ios::out );
-  if ( ! file_out ) {
-    l.log( "ERROR: cannot write file." );
-    return -1;
-  }
-
-  std::string                        a_line;
-  std::vector<std::string>           results;
-  std::vector<std::string>::iterator ri;
-  Context ctx(lc);
-
-  while( std::getline( file_in, a_line ) ) { 
-    if ( a_line == "" ) {
-      continue;
-    }
-
-    window_words_letters(a_line, lc, ctx, results);
-    for ( ri = results.begin(); ri != results.end(); ri++ ) {
-      file_out << *ri << "\n";
-    }
-    // Auto space after a line? Needs first word of next line
-    results.clear();
-  }
-  
-  file_out.close();
-  file_in.close();
-  
-  c.add_kv( "filename", output_filename );
-  l.log( "SET filename to "+output_filename );
-  return 0;
 }
 
 // Window a whoe text letter based. Treat text as one long stream of words
@@ -955,7 +1024,7 @@ int window_letters( Logfile& l, Config& c ) {
   }
 
   std::string                        a_word;
-  bool                               first = true;
+  bool                               first = true; //false to get first line with empty context
   std::vector<std::string>           results;
   std::vector<std::string>::iterator ri;
   Context ctx(lc);
@@ -963,7 +1032,13 @@ int window_letters( Logfile& l, Config& c ) {
   while ( file_in >> a_word ) { 
 
     if ( ! first ) {
-      window_word_letters( "_", a_word, lc, ctx, results );
+      //window_word_letters( "_", a_word, lc, ctx, results );
+      // We start by pushin a _ in the context and predicting the current
+      // word. We continue by windowing the rest of the current word
+      // normally. The last windowed prediction of each word predicts
+      // a _, therefore we need to push it in here.
+      ctx.push( "_" );
+      results.push_back( ctx.toString() + " " + a_word ); 
     }
     window_word_letters(a_word, a_word, lc, ctx, results);
     for ( ri = results.begin(); ri != results.end(); ri++ ) {
