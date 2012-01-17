5ca250706652016b759282aa971a0b462332ae37
pberck <pberck@12f355fe-0486-481a-ad91-c297ab22b4e3>
2012-01-17 12:40:37 +0000

    Script fixes.
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/wopr@14086 12f355fe-0486-481a-ad91-c297ab22b4e3

--- etc/watch_all.sh
+++ etc/watch_all.sh
@@ -9,16 +9,17 @@
 #
 MAILTO="P.J.Berck@UvT.nl"
 #
-LIMIT=99000000 
+LIMIT=99 #GB
 SLEEP=10
 #
 if test $# -lt 1
 then
-  echo "Supply LIMIT as argument."
+  echo "Supply LIMIT in GB as argument."
   exit
 fi
 #
-LIMIT=$1 # kB
+LIMIT=$1 
+LIMIT=`echo "$LIMIT * 1024 * 1024" | bc` #kB
 #
 DATE_CMD="date '+%s'"
 ECHO="-e"
@@ -30,9 +31,11 @@ while true;
   do
   #ps --no-header -p $PID -o etime,pid,rss,vsize,pcpu
   MEM=`eval ps -eo rss,comm,user | grep wopr | grep pberck | awk '{ SUM += $1} END { print SUM }'`
+  NUM=`ps -eo rss,comm,user | grep wopr | grep pberck | wc -l`
+  PERC=$(echo "scale=1; $MEM * 100 / $LIMIT" | bc)
   #
-  echo $ECHO "\r                       \c"
-  echo $ECHO "\rWoprs: $MEM \c"
+  echo $ECHO "\r                                           \c"
+  echo $ECHO "\rWoprs: $MEM ($NUM wopr(s), $PERC% of limit)\c"
   if test $MEM -gt $LIMIT
   then
     echo $ECHO "\n$MEM > $LIMIT"
