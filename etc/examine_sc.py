9294877dbe57f999daee96fe63a40b077f8a6186
pberck <pberck@12f355fe-0486-481a-ad91-c297ab22b4e3>
2013-06-05 10:13:32 +0000

    /hack
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/wopr@16197 12f355fe-0486-481a-ad91-c297ab22b4e3

--- etc/examine_sc.py
+++ etc/examine_sc.py
@@ -52,7 +52,13 @@ for scf in all_files:
         # but he had enough to learned (had) -14.0397 2.84644 16840.7 8 [ earned 1 ]
         # (.*)+ \(.*?\) (\d+) (\d+) (\d+) (\d+) \[ (.*?) (\d+) \]
         # ('let it not', 'pass', '-6.39197', '1.98777', '83.9797', '5', ' ')
-        # lp, entropy, word_lp, dist_size 
+        # lp, entropy, word_lp, dist_size
+        # 
+        logprob  = 0
+        entropy  = 0
+        wordlp   = 0
+        distsize = 0
+        matched = False
         m = re.match( "(.*?) \((.*?)\) ([0-9\.,e+\-]+) ([0-9\.,e+\-]+) ([0-9\.,e+\-]+) ([0-9\.,e+\-]+) \[ (.*?)\]", line )
         if m:
             #print m.groups()
@@ -61,25 +67,38 @@ for scf in all_files:
             entropy  = float(m.group(4))
             wordlp   = float(m.group(5))
             distsize = int(m.group(6))
-            sum_logprob += logprob
-            if logprob < min_logprob:
-                min_logprob = logprob
-            elif logprob > max_logprob:
-                max_logprob = logprob
-            if entropy < min_entropy:
-                min_entropy = entropy
-            elif entropy > max_entropy:
-                max_entropy = entropy
-            if distsize < min_distsize:
-                min_distsize = distsize
-            elif distsize > max_distsize:
-                max_distsize = distsize
-            if wordlp < min_wordlp:
-                min_wordlp = wordlp
-            elif wordlp > max_wordlp:
-                max_wordlp = wordlp
+            matched = True
         else:
-            if show_missed:
+            # instance+target classification log2prob entropy word_lp guess k/u md mal dist.cnt dist.sum RR ([topn])
+            # _ _ , the Instead Still -4.82762 7.70098 28.396 cd k 3 0 1056 5736 0.2 [ Still 412 Meanwhile 297 However 273 Indeed 223 Instead 202 ]
+            m = re.match( "(.*?) ([0-9\.,e+\-]+) ([0-9\.,e+\-]+) ([0-9\.,e+\-]+) (cd|cg|ic) (k|u) ([0-9\.,e+\-]+) ([0-9\.,e+\-]+) ([0-9\.,e+\-]+) ([0-9\.,e+\-]+) ([0-9\.,e+\-]+)( \[ (.*?)\])?", line)
+            if m:
+                line_count += 1
+                logprob  = float(m.group(2))
+                entropy  = float(m.group(3))
+                wordlp   = float(m.group(4))
+                distsize = int(m.group(9))
+                matched = True
+            if matched:
+                sum_logprob += logprob
+                if logprob < min_logprob:
+                    min_logprob = logprob
+                elif logprob > max_logprob:
+                    max_logprob = logprob
+                if entropy < min_entropy:
+                    min_entropy = entropy
+                elif entropy > max_entropy:
+                    max_entropy = entropy
+                if distsize < min_distsize:
+                    min_distsize = distsize
+                elif distsize > max_distsize:
+                    max_distsize = distsize
+                if wordlp < min_wordlp:
+                    min_wordlp = wordlp
+                elif wordlp > max_wordlp:
+                    max_wordlp = wordlp
+            
+            if not matched and show_missed:
                 print line[:-1] 
 
     print "File", scf
