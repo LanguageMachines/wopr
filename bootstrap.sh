c3faa3d7f42dd63ca2aa9a8a958d88b851940ffe
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2019-07-16 10:18:26 +0200

    Get ready for release

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,26 +20,13 @@ aclocal=aclocal
 
 # if you want to autogenerate a ChangeLog form svn:
 #
-#  svn2cl, a python script, as used in the GNU Enterprise project.
-#    By jcater (Jason Cater), contributions by reinhard (Reinhard Müller).
-#    Get it from
-#    http://www.gnuenterprise.org/cgi-bin/viewcvs.cgi/*checkout*/gnue/trunk/gnue-common/utils/svn2cl .
-#    svn2cl is used in Makefile.am too.
-#
-# (Another svn2cl implementation, in perl, is at
-# http://www.contactor.se/~dast/svn/archive-2002-04/0910.shtml)
-#
-# see also toplevel Makefile.am
-
-# test -f ChangeLog || {
-#   svn log --verbose > ChangeLog
-#}
+#  git2cl
 
 $aclocal -I m4
 
 
-if $automake --version|head -1 |grep ' 1\.[4-7]'; then
-    echo "automake 1.4-1.7 is active. You should use automake 1.8 or later"
+if $automake --version|head -1 |grep ' 1\.[4-8]'; then
+    echo "automake 1.4-1.8 is active. You should use automake 1.9 or later"
     if test -f /etc/debian_version; then
         echo " sudo apt-get install automake1.9"
         echo " sudo update-alternatives --config automake"
@@ -47,8 +34,8 @@ if $automake --version|head -1 |grep ' 1\.[4-7]'; then
     exit 1
 fi
 
-if $aclocal --version|head -1 |grep ' 1\.[4-7]'; then
-    echo "aclocal 1.4-1.7 is active. You should use aclocal 1.8 or later"
+if $aclocal --version|head -1 |grep ' 1\.[4-8]'; then
+    echo "aclocal 1.4-1.8 is active. You should use aclocal 1.9 or later"
     if test -f /etc/debian_version; then
         echo " sudo apt-get install aclocal1.9"
         echo " sudo update-alternatives --config aclocal"
